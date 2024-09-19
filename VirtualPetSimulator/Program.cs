d49b0b89ebaa817b0a0856beb06c11a5b9772933
Esteban Rosero <estebanrosero_95@hotmail.com>
2024-09-19 15:58:45 -0400

    third commit - final test

--- VirtualPetSimulator/Program.cs
+++ VirtualPetSimulator/Program.cs
@@ -25,8 +25,8 @@ while (true)
         Console.WriteLine("The number must be between 1 and 5.");
     }
 }
-Console.WriteLine("\n");
 Console.WriteLine("Congratulations! You've chosen a " + type + ". What would you like to name your pet?");
+Console.WriteLine("\n");
 while (true)
 {
     Console.WriteLine("Please insert the name of your " + type + ":");
@@ -40,67 +40,65 @@ while (true)
         Console.WriteLine("The name must contain letters only.");
     }
 }
-Console.WriteLine("\n");
 Console.WriteLine("Welcome, " + name + "! Let's start our adventure together!");
 Console.WriteLine("\n");
 int action = 0;
 int happiness = 5;
 int health = 5;
 int hunger = 5;
-Console.WriteLine("*** MAIN MENU ***");
-Console.WriteLine("1. Feed " + name);
-Console.WriteLine("2. Play with " + name);
-Console.WriteLine("3. Let " + name + " Rest");
-Console.WriteLine("4. Check " + name + " Status");
-Console.WriteLine("5. Exit");
-Console.WriteLine("\n");
 while (true)
 {
+    Console.WriteLine("*** MAIN MENU ***");
+    Console.WriteLine("1. Feed " + name);
+    Console.WriteLine("2. Play with " + name);
+    Console.WriteLine("3. Let " + name + " Rest");
+    Console.WriteLine("4. Check " + name + " Status");
+    Console.WriteLine("5. Exit");
+    Console.WriteLine("\n");
     Console.WriteLine("Please insert the number of your chosen action:");
     if (int.TryParse(Console.ReadLine(), out action) && action >= 1 && action <= 5)
     {
-        break;
+        switch (action)
+        {
+            case 1:
+                Console.WriteLine("You fed " + name + ". Its hunger decreases, and health improves slightly.");
+                Console.WriteLine("\n");
+                hunger -= 2;
+                health += 1;
+                break;
+            case 2:
+                Console.WriteLine("You played with " + name + ". Its happiness increases, but is a bit hungrier.");
+                Console.WriteLine("\n");
+                happiness += 2;
+                hunger += 1;
+                break;
+            case 3:
+                Console.WriteLine(name + " is resting. Its health improves, and happiness decreases slightly.");
+                Console.WriteLine("\n");
+                health += 3;
+                happiness -= 1;
+                break;
+            case 4:
+                happiness = happiness < 1 ? 1 : happiness > 10 ? 10 : happiness;
+                health = health < 1 ? 1 : health > 10 ? 10 : health;
+                hunger = hunger < 1 ? 1 : hunger > 10 ? 10 : hunger;
+                Console.WriteLine(name + "'s Status:");
+                Console.WriteLine("Happiness: " + happiness);
+                Console.WriteLine("Health: " + health);
+                Console.WriteLine("Hunger: " + hunger);
+                Console.WriteLine("\n");
+                break;
+            case 5:
+                Console.WriteLine("\n");
+                Console.WriteLine("Thank you for using the application. Have a good one!");
+                Environment.Exit(0);
+                break;
+            default:
+                break;
+        }
     }
     else
     {
         Console.WriteLine("The number must be between 1 and 5.");
     }
-}
-switch (action)
-{
-    case 1:
-        Console.WriteLine("\n");
-        Console.WriteLine("You fed " + name + ". Its hunger decreases, and health improves slightly.");
-        hunger -= 2;
-        health += 1;
-        break;
-    case 2:
-        Console.WriteLine("\n");
-        Console.WriteLine("You played with " + name + ". Its happiness increases, but is a bit hungrier.");
-        happiness += 2;
-        hunger += 1;
-        break;
-    case 3:
-        Console.WriteLine("\n");
-        Console.WriteLine(name + " is resting. Its health improves, and happiness decreases slightly.");
-        health += 3;
-        happiness -= 1;
-        break;
-    case 4:
-        happiness = happiness < 1 ? 1 : happiness > 10 ? 10 : happiness;
-        health = health < 1 ? 1 : health > 10 ? 10 : health;
-        hunger = hunger < 1 ? 1 : hunger > 10 ? 10 : hunger;
-        Console.WriteLine("\n");
-        Console.WriteLine(name + "'s Status:");
-        Console.WriteLine("Happiness: " + happiness);
-        Console.WriteLine("Health: " + health);
-        Console.WriteLine("Hunger: " + hunger);
-        break;
-    case 5:
-        Console.WriteLine("\n");
-        Console.WriteLine("Thank you for using the application. Have a good one!");
-        Environment.Exit(0);
-        break;
-    default:
-        break;
 }
